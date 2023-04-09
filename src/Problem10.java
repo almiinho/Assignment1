759ddb2e3d440b1de4de6ede2caa15814f4c1353
almiinho <almazazamatov863@gmail.com>
2023-04-10 00:20:33 +0600

    Problem1

--- src/Problem10.java
+++ src/Problem10.java
@@ -1,7 +1,7 @@
 public class Problem10 {
     public static int gcd(int a, int b) {
         if (b == 0) {
-            return a;
+            return a;//if a equals to 0 we just return 0
         } else
         {
             return gcd(b, a % b);
